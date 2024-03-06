b50847556b60d50e46ed89b30453bb915e25f2ed
Simpkins717 <dsimpkins717@gmail.com>
2024-03-05 20:38:06 -0500

    removed border on medium screens

--- KartRandomizer/src/App.tsx
+++ KartRandomizer/src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className='text-center align-middle min-h-screen font-barlowSemiCondensed sm:max-w-screen sm:flex sm:flex-col lg:max-w-7xl mx-auto'>
       <HeaderTwo />
-      <div className='  max-w-full mx-auto  pt-4 md:pt-12 pb-16 px-6 border-cyan-700 border-2 my-8 rounded-md'>
+      <div className='  max-w-full mx-auto  pt-4 md:pt-12 pb-16 px-6 md:border-0 border-cyan-700 border-2 my-8 rounded-md'>
         <Hero />
         <ToggleableSelectComponent />
       </div>
